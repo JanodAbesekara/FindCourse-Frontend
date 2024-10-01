0f0fc7458e369eb5bd6fdf514590fc2f6337db73
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-10-01 08:43:37 +0530

    save

--- find-course/src/Page/Adminpages.js
+++ find-course/src/Page/Adminpages.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Adminpages() {
+  const [feedback, setFeedback] = useState([]);
+
+  useEffect(() => {
+    try {
+      const response = axios.get(
+        `http://localhost:8080/api/V1/getallfeedbacks`,
+        {
+          withCredentials: true,
+        }
+      );
+      //setFeedback(response.data);
+      console.log(response);
+    } catch (error) {
+      console.error("Error fetching feedback data:", error);
+    }
+  }, []);
+
   return (
-    <div>Adminpages</div>
-  )
+    <div>
+      <h1>Admin page</h1>
+
+      <h3>Feed backs</h3>
+    </div>
+  );
 }
 
-export default Adminpages
+export default Adminpages;
